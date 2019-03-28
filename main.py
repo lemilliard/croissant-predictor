bb58d6f40eb34bf1f818801ce8eaad2a41786a28
Thomas Kint <thomaskint.pro@gmail.com>
2019-03-28 07:48:43 +0100

    Remise au propre, à améliorer

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import requests
 
 from selenium import webdriver
 
-hopla_url = 'https://hopla2.fr.capgemini.com'
-hopla_calendar_url = 'https://hopla2.fr.capgemini.com/Calendar'
-JSESSIONID_cookie = None
-JSESSIONID_cookie_name = 'JSESSIONID'
-brandNewDayProd_cookie = None
-brandNewDayProd_cookie_name = 'brandNewDayProd'
+hopla_url = "https://hopla2.fr.capgemini.com"
+hopla_calendar_url = "https://hopla2.fr.capgemini.com/Calendar"
+
+calendar_file = "calendar.json"
 
 
 class Person:
@@ -19,112 +17,108 @@ class Person:
         self.potential = 0
 
     def add_holiday(self, holiday):
-        datetime_object = datetime.datetime.strptime(holiday, '%Y-%m-%d')
         self.holiday.append(holiday)
 
     def is_in_holiday(self, date):
-        if date in self.holiday:
-            return True
-        else:
-            return False
+        return date in self.holiday
 
 
 class Solver:
-    @staticmethod
-    def fetch_cookie():
-        global JSESSIONID_cookie
-        global brandNewDayProd_cookie
+    def __init__(self):
+        self.cookies = []
+        # self.fetch_cookies()
+
+        self.calendar = {}
+        # self.fetch_calendar()
+        self.load_calendar()
+
+        self.persons = []
+        self.load_persons()
+
+    def fetch_cookies(self):
         options = webdriver.ChromeOptions()
         options.add_argument("mute-audio")
-        options.add_argument('disable-gpu')
-        options.add_argument('log-level=3')
-        options.add_argument('hide-scrollbars')
+        options.add_argument("disable-gpu")
+        options.add_argument("log-level=3")
+        options.add_argument("hide-scrollbars")
         options.add_argument("headless")
 
-        driver = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options)
+        driver = webdriver.Chrome(executable_path="./chromedriver.exe", chrome_options=options)
         driver.get(hopla_url)
-        JSESSIONID_cookie = driver.get_cookie(JSESSIONID_cookie_name)
-        brandNewDayProd_cookie = driver.get_cookie(brandNewDayProd_cookie_name)
+        self.cookies = driver.get_cookies()
         driver.close()
 
-    @staticmethod
-    def get_json_schedule():
+    def fetch_calendar(self):
         headers = {
-            'Content-type': 'application/x-www-form-urlencoded',
-            'Cookie': JSESSIONID_cookie_name + '=' + JSESSIONID_cookie[
-                'value'] + ';' + brandNewDayProd_cookie_name + '=' + brandNewDayProd_cookie['value']
+            "Content-type": "application/x-www-form-urlencoded",
+            "Cookie": "; ".join([cookie["name"] + "=" + cookie["value"] for cookie in self.cookies])
         }
         data = {
-            'startDate': '2019-03-01',
-            'collection': 'events',
-            'projectId': '5c735e75d5de9fb38da25c1a',
-            'nbMonths': '3',
-            'action': 'get'
+            "startDate": "2019-03-01",
+            "collection": "events",
+            "projectId": "5c735e75d5de9fb38da25c1a",
+            "nbMonths": "3",
+            "action": "get"
         }
         req = requests.post(hopla_calendar_url, headers=headers, data=data, verify=False)
-        with open('bam.json', 'wb') as f:
+        if req.status_code != 200:
+            raise Exception("Erreur dans la récupération des données")
+        with open(calendar_file, "wb") as f:
             f.write(req.content)
+        self.calendar = req.json()
 
-    @staticmethod
-    def determine_croissaniste(group, datetime_object):
+    def load_calendar(self):
+        with open(calendar_file) as f:
+            self.calendar = json.load(f)
+
+    def load_persons(self):
+        self.persons = [Person(person["first_name"] + " " + person["last_name"]) for person in self.calendar["users"]]
+        self.print_persons()
+
+    def determine_croissanistes(self, datetime_object):
         most_petential_person = None
         max_potential = 0
-        for person in group:
+        for person in self.persons:
             if not person.is_in_holiday(datetime_object) and person.potential >= max_potential:
                 max_potential = person.potential
                 most_petential_person = person
 
-        for person in group:
+        for person in self.persons:
             if person != most_petential_person:
                 person.potential += 1
             else:
                 person.potential = 0
 
-    @staticmethod
-    def print_array_info(array):
-        for person in array:
+    def print_persons(self):
+        for person in self.persons:
             print(person.name, person.potential)
 
-    @staticmethod
-    def print_all_holiday(array):
-        for person in array:
+    def print_holidays(self):
+        for person in self.persons:
             print(person.name, person.holiday)
 
-    @staticmethod
-    def load_json(filename):
-        with open(filename, 'r') as file:
-            return json.load(file)
+    def test(self):
+        datetime_object = datetime.datetime.strptime("2019-03-29", "%Y-%m-%d")
 
-    @classmethod
-    def solve_problem(cls):
-        cls.fetch_cookie()
-        print(JSESSIONID_cookie)
-        print(brandNewDayProd_cookie)
-        cls.get_json_schedule()
+        for i in range(len(self.persons)):
+            for j in range(31):
+                day = None
+                try:
+                    if "type" in self.calendar["users"][i]["days"][j].keys():
+                        day = self.calendar["users"][i]["days"][j]
+                except:
+                    day = self.calendar["users"][i]["days"][j][0]
+                if day is not None and day["type"] not in ["day_off", "RAS"]:
+                    self.persons[i].add_holiday(day["date"])
+
+        for i in range(5):
+            print("before")
+            self.print_persons()
+            self.determine_croissanistes(datetime_object.strftime("%Y-%m-%d"))
+            print("after")
+            self.print_persons()
+            datetime_object = datetime_object + datetime.timedelta(days=7)
 
 
-#
-# group = [Person('bob'), Person('alice'), Person('test')]
-# datetime_object = datetime.datetime.strptime('2019-03-29', '%Y-%m-%d')
-# json_schedule = load_json('planning.json')
-#
-# for i in range(len(group)):
-#     for j in range(31):
-#         day = None
-#         try:
-#             if 'type' in json_schedule['users'][i]['days'][j].keys():
-#                 day = json_schedule['users'][i]['days'][j]
-#         except:
-#             day = json_schedule['users'][i]['days'][j][0]
-#         if day != None and day['type'] not in ['day_off', 'RAS']:
-#             group[i].add_holiday(day['date'])
-#
-# for i in range(5):
-#     print('before')
-#     print_array_info(group)
-#     determine_croissaniste(group, datetime_object.strftime('%Y-%m-%d'))
-#     print('after')
-#     print_array_info(group)
-#     datetime_object = datetime_object + datetime.timedelta(days=7)
-
-Solver.solve_problem()
+solver = Solver()
+solver.test()
