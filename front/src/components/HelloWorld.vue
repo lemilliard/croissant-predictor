572af41c9770db2d5b6124467cbb7a6ce434cadd
BOUZAN <kevin.bouzan@capgemini.com>
2019-04-08 16:25:50 +0200

    modif front -  ajout init + loading

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,13 +1,11 @@
 <template>
   <v-flex xs8 id="app" style="margin: auto">
-    <v-btn
-      slot="activator"
+    <h1>CroissantPredictor</h1>
+    <v-progress-circular v-if="loading" style="margin-top: 50px;"
+      indeterminate
       color="primary"
-      dark
-      @click.native="loadProject">
-      Load
-    </v-btn>
-    <v-data-table
+    ></v-progress-circular>
+    <v-data-table style="margin-top: 50px;"
       :headers="headersProject"
       :items="dataProject"
       hide-actions
@@ -29,7 +27,7 @@
       </template>
     </v-data-table>
     <div>
-      <v-layout row wrap>
+      <v-layout row wrap style="margin-top: 20px; margin-bottom:20px">
         <v-flex xs4 id="id-textfield">
           <v-text-field
             id="id-field"
@@ -58,57 +56,61 @@
             ></v-text-field>
         </v-flex>
       </v-layout>
-      <v-btn
-        slot="activator"
-        color="primary"
-        dark
-        @click.native="loadPersons">
-        Persons
-      </v-btn>
-      <v-data-table
-        :headers="headersPersons"
-        :items="dataPersons"
-        hide-actions
-        class="elevation-1"
-        v-model="selected"
-        select-all
-        item-key="name"
-      >
-        <template slot="items" slot-scope="props">
-          <td>
-            <v-checkbox
-              v-model="props.selected"
-              primary
-              hide-details
-            ></v-checkbox>
-          </td>
-          <td>
-            {{ props.item.name }}
-          </td>
-        </template>
-      </v-data-table>
-      <v-flex xs12>
-        <v-btn
-          slot="activator"
-          color="primary"
-          dark
-          @click.native="predict">
-          Predict
-        </v-btn>
-      </v-flex>
-      <v-data-table
-      :headers="headers"
-      :items="data"
-      hide-actions
-      class="elevation-1"
-    >
-      <template slot="items" slot-scope="props">
-        <td v-for="(item, index) in props.item" :key="index">
-          <img v-if="index == 'picture'" :src="item"/>
-          <span v-else>{{ item }}</span>
-        </td>
-      </template>
-    </v-data-table>
+      <v-layout row wrap>
+        <v-flex xs4>
+          <v-btn v-if="value!=null"
+            slot="activator"
+            color="primary"
+            dark
+            @click.native="loadPersons">
+            Persons
+          </v-btn>
+          <v-data-table v-if="dataPersons.length > 0" style="margin-bottom:20px"
+            :headers="headersPersons"
+            :items="dataPersons"
+            hide-actions
+            class="elevation-1"
+            v-model="selected"
+            select-all
+            item-key="name"
+          >
+            <template slot="items" slot-scope="props">
+              <td>
+                <v-checkbox
+                  v-model="props.selected"
+                  primary
+                  hide-details
+                ></v-checkbox>
+              </td>
+              <td>
+                {{ props.item.name }}
+              </td>
+            </template>
+          </v-data-table>
+        </v-flex>
+        <v-flex xs8 style="padding-left:10px">
+          <v-btn v-if="value!=null"
+            slot="activator"
+            color="primary"
+            dark
+            @click.native="predict">
+            Predict
+          </v-btn>
+          <v-data-table v-if="data.length > 0"
+          :headers="headers"
+          :items="data"
+          hide-actions
+          class="elevation-1"
+          >
+            <template slot="items" slot-scope="props">
+              <td v-for="(item, index) in props.item" :key="index">
+                <img v-if="index == 'picture'" :src="item"/>
+                <span v-else>{{ item }}</span>
+              </td>
+            </template>
+          </v-data-table>
+        </v-flex>
+      </v-layout>
     </div>
   </v-flex>
 </template>
@@ -122,29 +124,34 @@ export default {
       nbCroissanist: 1,
       nbMonths: 1,
       headers: [
-        { text: 'Day', value: 'friday' },
-        { text: 'Name', value: 'name' },
-        { text: 'Picture', value: 'picture' },
+        { text: 'Day', value: 'friday', align: 'center' },
+        { text: 'Name', value: 'name', align: 'center' },
+        { text: 'Picture', value: 'picture', align: 'center' },
       ],
       headersProject: [
-        { text: 'ID Projet', value: 'id' },
-        { text: 'Nom', value: 'name' },
+        { text: 'ID Projet', value: 'id', align: 'center' },
+        { text: 'Nom', value: 'name', align: 'center' },
       ],
       headersPersons: [
-        { text: 'Name', value: 'name' },
+        { text: 'Name', value: 'name', align: 'center' },
       ],
       data: [],
       dataProject: [],
       dataPersons: [],
       selected: [],
       personSelected: [],
+      loading: false,
     };
+  },
+  created() {
+    this.loadProject();
   },
   methods: {
     onClickInput() {
       this.$emit('setLock', true);
     },
     predict() {
+      this.loading = true;
       this.personSelected = [];
       this.selected.forEach((e) => {
         this.personSelected.push(e.name);
@@ -153,39 +160,49 @@ export default {
         this.axios.post(`/define_croissanists_with_filter/${this.value}/${this.nbMonths}/${this.nbCroissanist}`, this.personSelected).then((response) => {
           if (response && response.data) {
             this.data = response.data;
+            this.loading = false;
           }
         }).catch(() => {
           this.response = 'Message invalide';
+          this.loading = false;
         });
       } else {
         this.axios.get(`/define_croissanists/${this.value}/${this.nbMonths}/${this.nbCroissanist}`).then((response) => {
           if (response && response.data) {
             this.data = response.data;
+            this.loading = false;
           }
         }).catch(() => {
           this.response = 'Message invalide';
+          this.loading = false;
         });
       }
     },
     loadProject() {
+      this.loading = true;
       this.axios.get('projects/load').then((response) => {
         if (response && response.data) {
           this.dataProject = response.data;
+          this.loading = false;
         }
       }).catch(() => {
         this.response = 'Message invalide';
+        this.loading = false;
       });
     },
     selectItem(item) {
       this.value = item.id;
     },
     loadPersons() {
+      this.loading = true;
       this.axios.get(`/persons/${this.value}/1`).then((response) => {
         if (response && response.data) {
           this.dataPersons = response.data;
+          this.loading = false;
         }
       }).catch(() => {
         this.response = 'Message invalide';
+        this.loading = false;
       });
     },
   },
